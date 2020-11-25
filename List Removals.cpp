21922980f6e8e9ffec7bbedc536caa200aee712c
Jonathan <jonathan.ryan.uy@gmail.com>
2020-11-25 05:38:13 -0500

    Updated List Removals

--- List Removals.cpp
+++ List Removals.cpp
@@ -1,15 +1,15 @@
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
-
+ 
 using namespace std;
 using namespace __gnu_pbds;
 const int maxN = 2e5;
-
+ 
 int N, p, x[maxN+1];
 tree<int, null_type, less<int>, rb_tree_tag,
 tree_order_statistics_node_update> T;
-
+ 
 int main(){
 	scanf("%d", &N);
 	for(int i = 1; i <= N; i++){
