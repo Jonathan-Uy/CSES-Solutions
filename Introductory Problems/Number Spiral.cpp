b8796ebd06598747ed06c0bb521c076ffb722aa2
Jonathan <jonathan.ryan.uy@gmail.com>
2020-11-26 10:42:17 -0500

    Moved Introductory Problems into folder

---
+++ Introductory Problems/Number Spiral.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+int T;
+ll X, Y;
+
+ll solve(ll x, ll y){
+	ll l = max(x, y)-1;
+	if(l&1){
+		if(x < y)   return l*l + x;
+		else        return l*l+2*l-y+2;
+	} else {
+		if(x < y)   return l*l+2*l-x+2;
+		else        return l*l + y;
+	}
+}
+
+int main(){
+	scanf("%d", &T);
+	for(int t = 0; t < T; t++){
+		scanf("%lld %lld", &X, &Y);
+		printf("%lld\n", solve(X, Y));
+	}
+}
---
+++ Introductory Problems/Weird Algorithm.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long N;
+
+int main(){
+	scanf("%lld", &N);
+	while(N > 1){
+		printf("%lld ", N);
+		if(N&1)	N = 3*N+1;
+		else 	N >>= 1;
+	}
+	printf("1\n");
+}
