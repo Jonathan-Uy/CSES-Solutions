937ae74829ecbf106f1d8c0882143d8f35c49df6
Jonathan <jonathan.ryan.uy@gmail.com>
2020-11-26 11:22:11 -0500

    Moved Range Queries to folder

---
+++ Range Queries/Range Sum Queries I.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+const int maxN = 2e5;
+
+int N, Q, a, b;
+ll x[maxN+1];
+
+int main(){
+	scanf("%d %d", &N, &Q);
+	for(int i = 1; i <= N; i++){
+		scanf("%lld", &x[i]);
+		x[i] += x[i-1];
+	}
+	for(int q = 0; q < Q; q++){
+		scanf("%d %d", &a, &b);
+		printf("%lld\n", x[b]-x[a-1]);
+	}
+}
