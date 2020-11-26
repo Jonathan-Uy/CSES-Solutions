67f15999831e3103910239229cfca417475de128
Jonathan <jonathan.ryan.uy@gmail.com>
2020-11-26 11:26:14 -0500

    Moved Tree Algorithms to folder

---
+++ Tree Algorithms/Distance Queries.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+const int maxN = 2e5+1;
+const int logN = 20;
+
+int N, Q, a, b, p[maxN][logN];
+int timer, d[maxN], in[maxN], out[maxN];
+vector<int> G[maxN];
+
+void dfs(int u = 1, int par = 1){
+	in[u] = ++timer;
+	d[u] = d[par]+1;
+	p[u][0] = par;
+	for(int i = 1; i < logN; i++)
+		p[u][i] = p[p[u][i-1]][i-1];
+	for(int v : G[u])
+		if(v != par)
+			dfs(v, u);
+	out[u] = ++timer;
+}
+
+bool ancestor(int u, int v){
+	return in[u] <= in[v] && out[u] >= out[v];
+}
+
+int lca(int u, int v){
+	if(ancestor(u, v))	return u;
+	if(ancestor(v, u))	return v;
+	for(int i = logN-1; i >= 0; i--)
+		if(!ancestor(p[u][i], v))
+			u = p[u][i];
+	return p[u][0];
+}
+
+int main(){
+	scanf("%d %d", &N, &Q);
+	for(int i = 0; i < N-1; i++){
+		scanf("%d %d", &a, &b);
+		G[a].push_back(b);
+		G[b].push_back(a);
+	}
+	
+	dfs();
+	
+	for(int q = 0; q < Q; q++){
+		scanf("%d %d", &a, &b);
+		printf("%d\n", d[a] + d[b] - 2*d[lca(a, b)]);
+	}
+}
---
+++ Tree Algorithms/Subtree Queries.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+int N, Q, a, b, t, s, timer;
+int in[200001], out[200001];
+ll k[200001], ds[400001];
+vector<int> G[200001];
+
+void update(int idx, ll val){
+    for(int i = idx; i <= 400000; i += i&-i)
+        ds[i] += val;
+}
+
+ll query(int idx){
+    ll sum = 0;
+    for(int i = idx; i > 0; i -= i&-i)
+        sum += ds[i];
+    return sum;
+}
+
+void dfs(int u, int p = 0){
+    in[u] = ++timer;
+    for(int v : G[u])
+        if(v != p)
+            dfs(v, u);
+    out[u] = ++timer;
+}
+
+int main(){
+    scanf("%d %d", &N, &Q);
+    for(int i = 1; i <= N; i++)
+        scanf("%lld", &k[i]);
+    
+    for(int i = 0; i < N-1; i++){
+        scanf("%d %d", &a, &b);
+        G[a].push_back(b);
+        G[b].push_back(a);
+    }
+    
+    timer = 0;
+    dfs(1);
+    
+    for(int i = 1; i <= N; i++)
+        update(out[i], k[i]);
+    
+    for(int q = 0; q < Q; q++){
+        scanf("%d %d", &t, &s);
+        if(t == 1){
+            update(out[s], -k[s]);
+            scanf("%lld", &k[s]);
+            update(out[s], k[s]);
+        } else if(t == 2){
+            printf("%lld\n", query(out[s])-query(in[s]));
+        }
+    }
+}
