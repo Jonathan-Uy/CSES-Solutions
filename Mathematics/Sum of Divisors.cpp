a5d512ddff091ded249e9fc69652f7dcb975e354
Jonathan <jonathan.ryan.uy@gmail.com>
2020-12-14 15:16:09 -0500

    Added Sum of Divisors

---
+++ Mathematics/Sum of Divisors.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+const ll MOD = 1e9+7;
+
+ll N, sum;
+
+ll sigma(ll x){
+	return ((x%MOD)*((x+1)%MOD)/2)%MOD;
+}
+
+int main(){
+	scanf("%lld", &N);
+	for(ll l = 1; l <= N; l++){
+		ll r = N/(N/l);
+		sum += (N/l)%MOD*(sigma(r)-sigma(l-1)+MOD)%MOD;
+		sum = (sum+MOD)%MOD;
+		l = r;
+	}
+	printf("%lld\n", sum);
+}
