8fd2def42f0d861bfd7ec64ba73e8211e5fecd4b
Jonathan <jonathan.ryan.uy@gmail.com>
2021-01-20 21:52:22 -0500

    Added Josephus Problems

---
+++ Sorting and Searching/Josephus Problem I.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+ 
+using namespace std;
+using namespace __gnu_pbds;
+
+int N, idx;
+tree<int, null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> T;
+
+int main(){
+	scanf("%d", &N);
+	for(int i = 1; i <= N; i++)
+		T.insert(i);
+	
+	idx = 1;
+	while(T.size()){
+		idx %= T.size();
+		int x = *T.find_by_order(idx);
+		T.erase(x);
+		printf("%d%c", x, (" \n")[T.size() == 0]);
+		idx++;
+	}
+}
---
+++ Sorting and Searching/Josephus Problem II.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+ 
+using namespace std;
+using namespace __gnu_pbds;
+
+int N, K, idx;
+tree<int, null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> T;
+
+int main(){
+	scanf("%d %d", &N, &K);
+	for(int i = 1; i <= N; i++)
+		T.insert(i);
+	
+	idx = K;
+	while(T.size()){
+		idx %= T.size();
+		int x = *T.find_by_order(idx);
+		T.erase(x);
+		printf("%d%c", x, (" \n")[T.size() == 0]);
+		idx += K;
+	}
+}
