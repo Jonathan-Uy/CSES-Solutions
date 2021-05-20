5ee75342941cbb4976ecb5b3894af077b8dc16dc
Jonathan <jonathan.ryan.uy@gmail.com>
2021-05-20 08:56:04 -0500

    Added Mail Delivery

---
+++ Graph Algorithms/Mail Delivery.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef pair<int,int> pii;
+const int maxN = 1e5+1;
+const int maxM = 2e5+1;
+
+int N, M, deg[maxN];
+bool tour_exists, used[maxM];
+vector<pii> G[maxN];
+vector<int> tour;
+stack<int> S;
+
+int main(){
+    scanf("%d %d", &N, &M);
+    for(int i = 0, a, b; i < M; i++){
+        scanf("%d %d", &a, &b);
+        G[a].push_back({b, i});
+        G[b].push_back({a, i});
+        deg[a]++; deg[b]++;
+    }
+
+    tour_exists = true;
+    for(int i = 1; i <= N; i++)
+        if(deg[i]&1)
+            tour_exists = false;
+
+    if(!tour_exists){
+        printf("IMPOSSIBLE\n");
+        return 0;
+    }
+
+    S.push(1);
+    while(!S.empty()){
+        int u = S.top();
+        if(deg[u]){
+            while(!G[u].empty()){
+                int v = G[u].back().first;
+                int id = G[u].back().second;
+                G[u].pop_back();
+
+                if(!used[id]){
+                    deg[u]--; deg[v]--;
+                    used[id] = true;
+                    S.push(v);
+                    break;
+                }
+            }
+        } else {
+            tour.push_back(u);
+            S.pop();
+        }
+    }
+
+    // Should be M+1 nodes in the Euler tour
+    // If not, it means the graph was not connected
+    if((int) tour.size() != M+1){
+        printf("IMPOSSIBLE\n");
+        return 0;
+    }
+
+    // Should be M+1 nodes in the Euler tour
+    for(int i = 0; i <= M; i++)
+        printf("%d%c", tour[i], (" \n")[i==M]);
+}
--- README.md
+++ README.md
@@ -4,14 +4,14 @@ Original solutions to the CSES Problem Set, written in C++.
 
 The CSES Problem Set is a collection of competitive programming practice problems. The goal of the project is to create a comprehensive high quality problem set for learning competitive programming. In January 2021, the collection was updated to include 300 problems! New problems will be gradually added. See the problem set [here](https://cses.fi/problemset/).
 
-As of May 19th, the following number of solutions have been completed:
+As of May 20th, the following number of solutions have been completed:
 
 | Problem Type          | Number Solved |
 |-----------------------|:-------------:|
 | Introductory Problems |     18/19     |
 | Sorting and Searching |     30/35     |
 | Dynamic Programming   |     18/19     |
-| Graph Algorithms      |     19/36     |
+| Graph Algorithms      |     20/36     |
 | Range Queries         |     19/19     |
 | Tree Algorithms       |     14/16     |
 | Mathematics           |     22/31     |
@@ -19,4 +19,4 @@ As of May 19th, the following number of solutions have been completed:
 | Geometry              |      6/7      |
 | Advanced Techniques   |     10/24     |
 | Additional Problems   |     16/77     |
-| **Total**             |  **173/300**  |
+| **Total**             |  **174/300**  |
