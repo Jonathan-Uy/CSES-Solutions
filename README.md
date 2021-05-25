b1312186ffc8fcd25803eb2dd8deb15d22c5e110
Jonathan Uy <jonathan.ryan.uy@gmail.com>
2021-05-25 15:30:42 -0500

    Added Fixed Length Paths I

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Original solutions to the CSES Problem Set, written in C++.
 
 The CSES Problem Set is a collection of competitive programming practice problems. The goal of the project is to create a comprehensive high quality problem set for learning competitive programming. In January 2021, the collection was updated to include 300 problems! New problems will be gradually added. See the problem set [here](https://cses.fi/problemset/).
 
-As of May 20th, the following number of solutions have been completed:
+As of May 25th, the following number of solutions have been completed:
 
 | Problem Type          | Number Solved |
 |-----------------------|:-------------:|
@@ -13,10 +13,10 @@ As of May 20th, the following number of solutions have been completed:
 | Dynamic Programming   |     18/19     |
 | Graph Algorithms      |     21/36     |
 | Range Queries         |     19/19     |
-| Tree Algorithms       |     14/16     |
+| Tree Algorithms       |     15/16     |
 | Mathematics           |     22/31     |
 | String Algorithms     |      1/17     |
 | Geometry              |      7/7      |
 | Advanced Techniques   |     10/24     |
 | Additional Problems   |     16/77     |
-| **Total**             |  **176/300**  |
+| **Total**             |  **177/300**  |
---
+++ Tree Algorithms/Fixed Length Paths I.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+const int maxN = 2e5+1;
+
+int N, K, deepest, sz[maxN], cnt[maxN];
+bool vis[maxN];
+ll ans;
+vector<int> G[maxN];
+
+int getSize(int u = 1, int p = -1){
+    sz[u] = 1;
+    for(int v : G[u])
+        if(v != p && !vis[v])
+            sz[u] += getSize(v, u);
+    
+    return sz[u];
+}
+
+int centroid(int u, int p, int n){
+    for(int v : G[u])
+        if(v != p && !vis[v] && sz[v] > n/2)
+            return centroid(v, u, n);
+
+    return u;
+}
+
+void dfs(int u, int p, bool t, int d = 1){
+    if(d > K)   return;
+
+    deepest = max(deepest, d);
+    if(t)       cnt[d]++;
+    else        ans += cnt[K-d];
+
+    for(int v : G[u])
+        if(v != p && !vis[v])
+            dfs(v, u, t, d+1);
+}
+
+void solve(int u = 1){
+    int c = centroid(u, -1, getSize(u));
+    vis[c] = true;
+    deepest = 0;
+
+    for(int v : G[c]){
+        if(!vis[v]){
+            dfs(v, c, false);
+            dfs(v, c, true);
+        }
+    }
+    fill(cnt+1, cnt+deepest+1, 0);
+
+    for(int v : G[c])
+        if(!vis[v])
+            solve(v);
+}
+
+int main(){
+    scanf("%d %d", &N, &K);
+    for(int i = 0, a, b; i < N-1; i++){
+        scanf("%d %d", &a, &b);
+        G[a].push_back(b);
+        G[b].push_back(a);
+    }
+
+    cnt[0] = 1;
+    solve();
+    printf("%lld\n", ans);
+}
