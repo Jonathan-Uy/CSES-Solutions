4da01210b88d4bcbf32893bda76dce3d01fd1573
Jonathan Uy <jonathan.ryan.uy@gmail.com>
2021-07-16 05:57:06 -0400

    Added Hamiltonian Flights

---
+++ Graph Algorithms/Hamiltonian Flights.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+const int maxN = 20;
+const int SIZE = 1<<maxN;
+const ll MOD = 1e9+7;
+
+int N, M;
+bool inqueue[maxN][SIZE];
+ll dp[maxN][SIZE];
+vector<int> G[maxN];
+queue<pii> Q;
+
+int main(){
+    scanf("%d %d", &N, &M);
+    for(int i = 0, a, b; i < M; i++){
+        scanf("%d %d", &a, &b);
+        G[a-1].push_back(b-1);
+    }
+    
+    dp[0][1] = 1;
+    Q.push({0, 1});
+    inqueue[0][1] = true;
+    while(!Q.empty()){
+        int u = Q.front().first;
+        int mask = Q.front().second;
+        Q.pop();
+        
+        if(u != N-1){
+            for(int v : G[u]){
+                int newMask = mask|(1<<v);
+                if((mask&(1<<v)) == 0){
+                    dp[v][newMask] += dp[u][mask];
+                    dp[v][newMask] %= MOD; 
+                    if(!inqueue[v][newMask]){
+                        Q.push({v, newMask});
+                        inqueue[v][newMask] = true;
+                    }
+                }
+            }
+        }
+    }
+    
+    printf("%lld\n", dp[N-1][(1<<N)-1]);
+}
