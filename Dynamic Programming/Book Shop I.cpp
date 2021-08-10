5898ed7319ab0e274adee6057429be565f20382e
Jonathan-Uy <jonathan.ryan.uy@gmail.com>
2021-08-10 02:16:17 -0500

    Deleted unnecessary braces

--- Dynamic Programming/Book Shop I.cpp
+++ Dynamic Programming/Book Shop I.cpp
@@ -12,11 +12,10 @@ int main(){
     for(int i = 0; i < N; i++)  scanf("%d", &s[i]);
 
     fill(dp+1, dp+X+1, -1);
-    for(int i = 0; i < N; i++){
+    for(int i = 0; i < N; i++)
         for(int j = X-h[i]; j >= 0; j--)
             if(dp[j] != -1)
                 dp[j+h[i]] = max(dp[j+h[i]], dp[j]+s[i]);
-    }
 
     for(int i = 1; i <= X; i++)
         dp[i] = max(dp[i], dp[i-1]);
