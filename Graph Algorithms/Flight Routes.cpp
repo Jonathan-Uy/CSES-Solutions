7956b064c2b0cbb3dc0ea333552d835ba2592447
Jonathan-Uy <jonathan.ryan.uy@gmail.com>
2021-08-26 18:50:56 -0500

    Added Flight Routes

---
+++ Graph Algorithms/Flight Routes.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int,ll> edge;
+typedef pair<ll,int> node;
+const int maxN = 2e5+1;
+
+int N, M, K, cnt[maxN];
+vector<edge> G[maxN];
+priority_queue<node, vector<node>, greater<node>> Q;
+
+int main(){
+    scanf("%d %d %d", &N, &M, &K);
+    for(int i = 0, a, b, c; i < M; i++){
+        scanf("%d %d %d", &a, &b, &c);
+        G[a].push_back({b, c});
+    }
+
+    Q.push({0, 1});
+    while(!Q.empty()){
+        ll d = get<0>(Q.top());
+        int u = get<1>(Q.top());
+        Q.pop();
+
+        cnt[u]++;
+        if(u == N){
+            printf("%lld%c", d, (" \n")[cnt[u]==K]);
+            if(cnt[u] == K) return 0;
+        }
+        
+        if(cnt[u] <= K){
+            for(edge e : G[u]){
+                int v = e.first;
+                ll w = e.second;
+                Q.push({d+w, v});
+            }
+        }
+    }
+}
