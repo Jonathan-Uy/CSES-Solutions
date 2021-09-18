1d33ea04b4f062eba05d9a207a3b5c5f9168feb0
Jonathan-Uy <jonathan.ryan.uy@gmail.com>
2021-09-18 04:25:48 -0500

    Modified Counting Bits

--- Additional Problems/Counting Bits.cpp
+++ Additional Problems/Counting Bits.cpp
@@ -4,14 +4,13 @@ using namespace std;
 typedef long long ll;
 
 ll N;
-map<ll,ll> memo;
+map<ll,ll> dp;
 
 ll f(ll a){
     if(a == 0)  return 0;
-    if(memo[a]) return memo[a];
-    if(a&1) memo[a] = 2*f(a/2)+(a/2)+1;
-    else    memo[a] = f(a/2)+f(a/2-1)+(a/2);
-    return  memo[a];
+    if(dp[a])   return dp[a];
+    if(a&1)     return dp[a] = 2*f(a/2)+(a/2)+1;
+    else        return dp[a] = f(a/2)+f(a/2-1)+(a/2);
 }
 
 int main(){
