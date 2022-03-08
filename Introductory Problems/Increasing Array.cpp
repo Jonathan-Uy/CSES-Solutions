d8a28fd474461ce4bb2b20af820a1f6a41cec850
Angraj <67864122+AnugrahYadav@users.noreply.github.com>
2022-03-08 23:52:06 +0530

    while practicing I find minor bug so is corrected it line : 12, (i<N-1)
    Hey !! It's Anugrah First of all thank you for such a helpful repository and there was a small bug in the code which I debugged ... while practicing

--- Introductory Problems/Increasing Array.cpp
+++ Introductory Problems/Increasing Array.cpp
@@ -9,7 +9,7 @@ ll x, mx, sum;
 int main(){
     scanf("%d", &N);
     scanf("%lld", &mx);
-    for(int i = 1; i < N; i++){
+    for(int i = 1; i < N-1; i++){
         scanf("%lld", &x);
         mx = max(mx, x);
         sum += (mx - x);
