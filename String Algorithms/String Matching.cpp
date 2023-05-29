d405fffd6e55c9d34e05ddfd7cd2414d6701ea78
Jonathan Uy <61035334+Jonathan-Uy@users.noreply.github.com>
2023-05-29 23:28:35 +0100

    Prevent collision: hash with some list of primes

--- String Algorithms/String Matching.cpp
+++ String Algorithms/String Matching.cpp
@@ -2,14 +2,20 @@
 
 using namespace std;
 typedef long long ll;
-const int maxN = 1e6+5;
+const int maxN = 1e6+5, numP = 3;
 const ll MOD = 1e9+7;
-const ll p1 = 31;
-const ll p2 = 37;
+const ll prime[numP] = {31, 37, 43};
 
 int N, M, cnt;
-ll sh1, sh2, th1, th2, pow1[maxN], pow2[maxN];
+ll sh[numP], th[numP], pw[numP][maxN];
 char S[maxN], T[maxN];
+
+bool hashes_equal(){
+    for(int p = 0; p < numP; p++)
+        if(sh[p] != th[p])
+            return false;
+    return true;
+}
 
 int main(){
     scanf("%s %s", S, T);
@@ -20,26 +26,26 @@ int main(){
         return 0;
     }
 
-    pow1[0] = pow2[0] = 1;
-    for(int i = 1; i < maxN; i++){
-        pow1[i] = (pow1[i-1] * p1) % MOD;
-        pow2[i] = (pow2[i-1] * p2) % MOD;
+    for(int p = 0; p < numP; p++){
+        pw[p][0] = 1;
+        for(int i = 1; i < maxN; i++)
+            pw[p][i] = (pw[p][i-1] * prime[p]) % MOD;
     }
 
     for(int i = 0; i < M; i++){
-        sh1 = (sh1 + (S[i] - 'a' + 1) * pow1[M-i-1]) % MOD;
-        sh2 = (sh2 + (S[i] - 'a' + 1) * pow2[M-i-1]) % MOD;
-        th1 = (th1 + (T[i] - 'a' + 1) * pow1[M-i-1]) % MOD;
-        th2 = (th2 + (T[i] - 'a' + 1) * pow2[M-i-1]) % MOD;
+        for(int p = 0; p < numP; p++){
+            sh[p] = (sh[p] + (S[i] - 'a' + 1) * pw[p][M-i-1]) % MOD;
+            th[p] = (th[p] + (T[i] - 'a' + 1) * pw[p][M-i-1]) % MOD;
+        }
     }
 
-    if(sh1 == th1 && sh2 == th2)    cnt++;
+    if(hashes_equal())  cnt++;
     for(int i = M; i < N; i++){
-        sh1 = (sh1 * p1 - (S[i-M] - 'a' + 1) * pow1[M]) % MOD;
-        sh1 = (sh1 + (S[i] - 'a' + 1) + MOD) % MOD;
-        sh2 = (sh2 * p2 - (S[i-M] - 'a' + 1) * pow2[M]) % MOD;
-        sh2 = (sh2 + (S[i] - 'a' + 1) + MOD) % MOD;
-        if(sh1 == th1 && sh2 == th2)    cnt++;
+        for(int p = 0; p < numP; p++){
+            sh[p] = (sh[p] * prime[p] - (S[i-M] - 'a' + 1) * pw[p][M]) % MOD;
+            sh[p] = (sh[p] + (S[i] - 'a' + 1) + MOD) % MOD;
+        }
+        if(hashes_equal())  cnt++;
     }
     printf("%d\n", cnt);
 }
