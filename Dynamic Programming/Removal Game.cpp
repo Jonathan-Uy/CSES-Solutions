f2704418be0473f3125a0cb13c5c0dc573edf88c
Michael Hayter <mehayter@gmail.com>
2023-06-15 06:32:16 -0400

    Update Removal Game.cpp - 0 is valid so memoization wasn't working
    Tested! Thanks for the repo!

--- Dynamic Programming/Removal Game.cpp
+++ Dynamic Programming/Removal Game.cpp
@@ -6,14 +6,16 @@ const int maxN = 5000;
 
 int N, x[maxN+1];
 ll p[maxN+1], dp[maxN+1][maxN+1];
+bool found[maxN+1][maxN+1];
 
 ll sum(int l, int r){
 	return p[r] - p[l-1];
 }
 
 ll solve(int l, int r){
-	if(dp[l][r])	return dp[l][r];
+	if(found[l][r])	return dp[l][r];
 	if(l == r)		return x[l];
+	found[l][r]=true;
 	return dp[l][r] = max(x[l]+sum(l+1, r)-solve(l+1, r), x[r]+sum(l,r-1)-solve(l,r-1));
 }
 
